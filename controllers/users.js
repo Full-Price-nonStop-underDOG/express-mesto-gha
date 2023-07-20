1484a4a42e07152105bec105e8d7a399dbab015f
full-price-nonstop-underdog <korvin.korp@yandex.ru>
2023-07-20 15:47:47 +0300

    test3

--- controllers/users.js
+++ controllers/users.js
@@ -3,43 +3,50 @@ const router = express.Router();
 const User = require("../models/user");
 
 // GET /users - возвращает всех пользователей
-module.exports.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch users" });
-  }
-});
+module.exports.getUsers =
+  ("/users",
+  async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch users" });
+    }
+  });
 
 // GET /users/:userId - возвращает пользователя по _id
-module.exports.getById("/users/:userId", async (req, res) => {
-  const { userId } = bodyParserreq.params;
-  try {
-    const user = await User.findById(userId);
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ error: "User not found" });
+module.exports.getById =
+  ("/users/:userId",
+  async (req, res) => {
+    const { userId } = bodyParserreq.params;
+    try {
+      const user = await User.findById(userId);
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ error: "User not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch user" });
     }
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch user" });
-  }
-});
+    п;
+  });
 
 // POST /users - создаёт пользователя
-module.exports.createUser("/users", async (req, res) => {
-  const { name, about, avatar } = req.body;
-  console.log(req.body);
-  try {
-    const newUser = await User.create({ name, about, avatar });
-    console.log(newUser);
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Failed to create user" });
-  }
-});
+module.exports.createUser =
+  ("/users",
+  async (req, res) => {
+    const { name, about, avatar } = req.body;
+    console.log(req.body);
+    try {
+      const newUser = await User.create({ name, about, avatar });
+      console.log(newUser);
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Failed to create user" });
+    }
+  });
 
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {
-  get,
+  getUsers,
   getById,
   createUser,
   updateProfile,
