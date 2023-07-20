1871e727a35e6afe09525d67e8e3c6fd046b39a3
full-price-nonstop-underdog <korvin.korp@yandex.ru>
2023-07-20 16:05:17 +0300

    test4

--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,47 @@ const {
 
 // PATCH /users/me — обновляет профиль
 router.patch("/users/me", updateProfile);
-router.patch("/users", createUser);
 
 // PATCH /users/me/avatar — обновляет аватар
 router.patch("/users/me/avatar", updateAvatar);
+
+// GET /users - возвращает всех пользователей
+router.get("/users", async (req, res) => {
+  try {
+    const users = await getUsers(); // Используем функцию getUsers, которую импортировали из контроллера
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
+
+// GET /users/:userId - возвращает пользователя по _id
+router.get("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await getById(userId); // Используем функцию getById из контроллера
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch user" });
+  }
+});
+
+// POST /users - создаёт пользователя
+router.post("/users", async (req, res) => {
+  const { name, about, avatar } = req.body;
+  console.log(req.body);
+  try {
+    const newUser = await createUser(name, about, avatar); // Используем функцию createUser из контроллера
+    console.log(newUser);
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to create user" });
+  }
+});
 
 module.exports = router;
