4c1d2191a2eb51f1f5b476211d44a6f15da6f9e3
full-price-nonstop-underdog <korvin.korp@yandex.ru>
2023-07-22 17:07:51 +0300

    t19

--- controllers/users.js
+++ controllers/users.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const router = express.Router();
 const User = require("../models/user");
+const mongoose = require("mongoose");
+
+const ERROR_CODE = 400;
+const ERROR_CODE_NOT_FOUND = 404;
 
 // GET /users - возвращает всех пользователей
-module.exports.getUsers =
-  ("/users",
-  async (req, res) => {
-    try {
-      const users = await User.find();
-      res.json(users);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to fetch users" });
-    }
-  });
+module.exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+};
 
 // GET /users/:userId - возвращает пользователя по _id
 module.exports.getById = async (req, res) => {
   const { userId } = req.params;
   try {
     if (!mongoose.Types.ObjectId.isValid(userId)) {
-      return res.status(400).json({ error: "Invalid user ID" });
+      return res.status(ERROR_CODE).json({ error: "Invalid user ID" });
     }
 
     const user = await User.findById(userId);
     if (user) {
       res.json(user);
     } else {
-      res.status(404).json({ error: "User not found" });
+      res.status(ERROR_CODE_NOT_FOUND).json({ error: "User not found" });
     }
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch user" });
@@ -34,24 +34,50 @@ module.exports.getById = async (req, res) => {
 };
 
 // POST /users - создаёт пользователя
-// Ваш код createUser
-// POST /users - создаёт пользователя
-// module.exports.createUser = async (req, res) => {
-//   const { name, about, avatar } = req.body;
-//   console.log(req.body);
+module.exports.createUser = async (req, res) => {
+  const { name, about, avatar } = req.body;
+  console.log(req.body);
 
-//   // Добавьте проверку на длину поля name
+  try {
+    if (!name || name.trim().length === 0) {
+      return res
+        .status(ERROR_CODE)
+        .json({ error: "Name is required and should not be empty" });
+    }
 
-//   try {
-//     const newUser = await User.create({ name, about, avatar });
-//     console.log(newUser);
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "Failed to create user" });
-//   }
-// };
+    if (name.length < 2 || name.length > 30) {
+      return res.status(ERROR_CODE).json({
+        error: "Name should be between 2 and 30 characters long",
+        message: "Failed to create username",
+      });
+    }
 
+    if (about.length < 2 || about.length > 30) {
+      return res.status(ERROR_CODE).json({
+        error: "About should be between 2 and 30 characters long",
+        message: "Failed to create user3",
+      });
+    }
+
+    if (!avatar || avatar.trim().length === 0) {
+      return res
+        .status(ERROR_CODE)
+        .json({ error: "Avatar is required and should not be empty" });
+    }
+
+    const newUser = await User.create({ name, about, avatar });
+    console.log(newUser);
+    res.status(201).json(newUser);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(ERROR_CODE).json({ message: error.message });
+    }
+    console.log(error);
+    res.status(500).json({ error: "Failed to create user" });
+  }
+};
+
+// PATCH /users/me — обновляет профиль
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
   const userId = req.user._id;
@@ -65,7 +91,7 @@ module.exports.updateProfile = (req, res) => {
       if (updatedUser) {
         res.json(updatedUser);
       } else {
-        res.status(404).json({ error: "User not found" });
+        res.status(ERROR_CODE_NOT_FOUND).json({ error: "User not found" });
       }
     })
     .catch((error) => {
@@ -83,10 +109,37 @@ module.exports.updateAvatar = (req, res) => {
       if (updatedUser) {
         res.json(updatedUser);
       } else {
-        res.status(404).json({ error: "User not found" });
+        res.status(ERROR_CODE_NOT_FOUND).json({ error: "User not found" });
       }
     })
     .catch((error) => {
       res.status(500).json({ error: "Failed to update avatar" });
     });
 };
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const userId = req.user._id;
+
+  User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
+    .then((updatedUser) => {
+      if (updatedUser) {
+        res.json(updatedUser);
+      } else {
+        res.status(ERROR_CODE_NOT_FOUND).json({ error: "User not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Failed to update avatar" });
+    });
+};
+
+module.exports.likeCard = async (req, res) => {
+  // ... (rest of the code for liking a card)
+};
+
+module.exports.dislikeCard = async (req, res) => {
+  // ... (rest of the code for disliking a card)
+};
+
+// ... (rest of the code for other user-related functionalities, if any)
--- routes/users.js
+++ routes/users.js
@@ -1,84 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
 const {
   getUsers,
   getById,
-
+  createUser,
   updateProfile,
   updateAvatar,
 } = require("../controllers/users");
+
+// GET /users - возвращает всех пользователей
+router.get("/users", getUsers);
+
+// GET /users/:userId - возвращает пользователя по _id
+router.get("/users/:userId", getById);
+
+// POST /users - создаёт пользователя
+router.post("/users", createUser);
 
 // PATCH /users/me — обновляет профиль
 router.patch("/users/me", updateProfile);
 
 // PATCH /users/me/avatar — обновляет аватар
 router.patch("/users/me/avatar", updateAvatar);
-
-// GET /users - возвращает всех пользователей
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch users" });
-  }
-});
-
-// GET /users/:userId - возвращает пользователя по _id
-router.get("/users/:userId", async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const user = await User.findById(userId);
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ error: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch user" });
-  }
-});
-
-// POST /users - создаёт пользователя
-router.post("/users", async (req, res) => {
-  const { name, about, avatar } = req.body;
-  console.log(req.body);
-
-  try {
-    if (!name || name.trim().length === 0) {
-      return res
-        .status(400)
-        .json({ error: "Name is required and should not be empty" });
-    }
-
-    if (name.length < 2 || name.length > 30) {
-      return res.status(400).json({
-        error: "Name should be between 2 and 30 characters long",
-        message: "Failed to create username",
-      });
-    }
-
-    if (about.length < 2 || about.length > 30) {
-      return res.status(400).json({
-        error: "About should be between 2 and 30 characters long",
-        message: "Failed to create user3",
-      });
-    }
-
-    if (!avatar || avatar.trim().length === 0) {
-      return res
-        .status(400)
-        .json({ error: "Avatar is required and should not be empty" });
-    }
-
-    const newUser = await User.create({ name, about, avatar });
-    console.log(newUser);
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Failed to create user" });
-  }
-});
 
 module.exports = router;
