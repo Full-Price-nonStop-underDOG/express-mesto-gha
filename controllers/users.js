4d6a89fabdd25649a7105f097dd64c730a287feb
full-price-nonstop-underdog <korvin.korp@yandex.ru>
2023-07-24 22:05:07 +0300

    u

--- controllers/users.js
+++ controllers/users.js
@@ -117,7 +117,7 @@ module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
     .then((updatedUser) => {
       if (updatedUser) {
-        res.json(updatedUser);
+        res.type("text/html; charset=utf-8").json(updatedUser);
       } else {
         res.status(ERROR_CODE_NOT_FOUND).json({ message: "User not found" });
       }
