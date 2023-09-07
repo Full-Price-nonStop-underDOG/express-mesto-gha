11c8d284c7d74b2d1e05150ff756b49de3c3842a
full-price-nonstop-underdog <korvin.korp@yandex.ru>
2023-09-07 23:31:40 +0300

    secret

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,21 @@
 const jwt = require('jsonwebtoken');
+const TokenInvalidError = require('../errors/tokenInvalidError');
 
 module.exports = (req, res, next) => {
   const { authorization: bearerToken } = req.headers;
 
   if (!bearerToken) {
-    return res.status(401).send({ message: 'Необходима авторизация1' });
+    return next(new TokenInvalidError('Необходима авторизация1'));
   }
 
   const token = bearerToken.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'your-secret-key');
+    const jwtSecret = process.env.JWT_SECRET; // Получаем секретный ключ из переменных окружения
+    payload = jwt.verify(token, jwtSecret); // Используем секретный ключ из переменных окружения
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация2' });
+    return next(new TokenInvalidError('Необходима авторизация2'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
